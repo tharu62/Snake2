ce4ccf64ebf9e87a584b7a9df79fc8512503c006
tharu62 <tharinduxdeshan@gmail.com>
2025-03-13 19:39:41 +0100

    basics moves done

--- snake2.py
+++ snake2.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 from pygame.locals import *
 from enum import Enum
 
@@ -11,10 +12,11 @@ class Direction(Enum):
 
 class Snake():
     head = Rect
-    body = [Rect((389, 299, 9, 9)), Rect(379, 299, 9, 9), Rect((369, 299, 9, 9))]
+    body = []
 
     def __init__(self, head):
         self.head = head
+        self.body.append(Rect((head.x+10, head.y+10, 9, 9)))
 
     def move(self, direction):
         self.body[0].x = self.head.x
@@ -33,9 +35,7 @@ class Snake():
                 self.head.move_ip(10, 0)
 
     def eat(self, direction):
-        tail = Rect((209, 209, 9 ,9))
-        tail.x = self.body[len(self.body)-1].x
-        tail.y = self.body[len(self.body)-1].y
+        tail = Rect((self.body[len(self.body)-1].x, self.body[len(self.body)-1].y, 9 ,9))
         match direction:
             case Direction.UP:
                 tail.move_ip(0, 10)
@@ -47,24 +47,39 @@ class Snake():
                 tail.move_ip(-10, 0)
         self.body.append(tail)
 
-    # def out_of_bound(self, head):
-    #     if head.x >
+    def out_of_bound(self):
+        if self.head.x == 400 or self.head.x == 0:
+            return True
+        if self.head.y == 590 or self.head.y == 0:
+            return True
+        return False
+    
+    def bonk(self):
+        for i in self.body:
+            if self.head.x == i.x and self.head.y == i.y:
+                return True
+        return False
+    
+def random_apple():
+    apple = pygame.Rect((0, 0, 9, 9))
+    apple.move_ip(random.randint(0,3)*10, random.randint(0, 7)*10)
+    return apple     
 
 
 pygame.init()
 
-SCREEN_WIDHT = 831
+SCREEN_WIDHT = 810
 SCREEN_HIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HIGHT))
 
-green_snake = Snake(pygame.Rect((399, 299, 9, 9))) 
-apple = pygame.Rect(((209, 209, 9, 9)))
+green_snake = Snake(pygame.Rect((190, 290, 9, 9))) 
+apple = pygame.Rect(((200, 200, 9, 9)))
 wall = []
 
 # Set up the player and the apple on screen
-for i in range(0, 59):
-    temp = pygame.Rect((411, 0, 10, 10))
+for i in range(0, 60):
+    temp = pygame.Rect((400, 0, 9, 9))
     temp.move_ip(0, 10*i)
     wall.append(temp)
 pygame.draw.rect(screen, (255, 0, 0), apple)
@@ -72,13 +87,15 @@ pygame.draw.rect(screen, (255, 255, 0), green_snake.head)
 for i in green_snake.body:
     pygame.draw.rect(screen, (0, 255, 0), i)
 dir = Direction.RIGHT
+update_time = 0.05
 
 # Game Loop
 run = True
 while run:
 
     screen.fill((0,0,0))
-    
+    update_time = 0.05
+
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] == True:
         dir = Direction.LEFT
@@ -92,6 +109,8 @@ while run:
     elif key[pygame.K_DOWN] == True:
         dir = Direction.DOWN
         green_snake.move(dir)
+    elif key[pygame.K_SPACE] == True:
+        update_time = 1
     else:
         green_snake.move(dir)
 
@@ -99,21 +118,21 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     
-    # if green_snake
+    if green_snake.out_of_bound() or green_snake.bonk():
+        run = False
 
     if green_snake.head.x == apple.x and green_snake.head.y == apple.y:
-        green_snake.eat(dir)   
+        green_snake.eat(dir)
+        apple = random_apple()   
 
     pygame.draw.rect(screen, (255, 0, 0), apple)
     pygame.draw.rect(screen, (255, 255, 0), green_snake.head)
     for i in green_snake.body:
         pygame.draw.rect(screen, (0, 255, 0), i)
-
-    for i in range(0,59):
+    for i in range(0,60):
         pygame.draw.rect(screen, (0, 0, 255), wall[i])    
 
     pygame.display.update()
     time.sleep(0.05)
-
 pygame.quit()
 
