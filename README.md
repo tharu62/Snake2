a22884402093beeef4929f6175558419ca8f4318
tharu62 <tharinduxdeshan@gmail.com>
2025-03-13 22:19:41 +0100

    resolved framerate issue and added score

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Snake_pyGame
+# Snake2
--- snake2.py
+++ snake2.py
@@ -46,11 +46,11 @@ class Snake():
             case Direction.RIGHT:
                 tail.move_ip(-10, 0)
         self.body.append(tail)
-
+    
     def out_of_bound(self):
         if self.head.x == 400 or self.head.x == 0:
             return True
-        if self.head.y == 590 or self.head.y == -10:
+        if self.head.y == 530 or self.head.y == -10:
             return True
         return False
     
@@ -62,11 +62,13 @@ class Snake():
     
 def random_apple():
     apple = pygame.Rect((0, 0, 9, 9))
-    apple.move_ip(random.randint(1,3)*10, random.randint(1, 7)*10)
+    apple.move_ip(random.randint(1,30)*10, random.randint(1, 50)*10)
     return apple     
 
 
 pygame.init()
+
+timer = pygame.time.Clock()
 
 SCREEN_WIDHT = 810
 SCREEN_HIGHT = 600
@@ -87,7 +89,7 @@ for i in range(61, 143):
     temp.move_ip(10*(i-61), 10)
     wall.append(temp)
 for i in range(143, 226):
-    temp = pygame.Rect((0, 590, 9, 9))
+    temp = pygame.Rect((0, 530, 9, 9))
     temp.move_ip(10*(i-143), 0)
     wall.append(temp)
 for i in range(226, 287):
@@ -98,6 +100,10 @@ for i in range(287, 348):
     temp = pygame.Rect((800, 0, 9, 9))
     temp.move_ip(0, 10*(i-287))
     wall.append(temp)
+for i in range(349, 430):
+    temp = pygame.Rect((0, 590, 9, 9))
+    temp.move_ip(10*(i-349), 0)
+    wall.append(temp)    
 
 # Set up the player and the apple on screen
 pygame.draw.rect(screen, (255, 0, 0), apple)
@@ -107,13 +113,21 @@ for i in green_snake.body:
 dir = Direction.RIGHT
 update_time = 0.05
 
+# Set up the font for rendering the score
+font = pygame.font.SysFont(None, 35)
+yellow = (255, 255, 0)
+black = (0, 0, 0)
+
 # Game Loop
 run = True
-FRAME_COUNT = 0
+FRAMERATE = 20
+PAUSE = 0
+SCORE = 0
 while run:
 
-    screen.fill((0,0,0))
+    timer.tick(FRAMERATE)
 
+    screen.fill((0,0,0))
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] == True:
         dir = Direction.LEFT
@@ -123,32 +137,36 @@ while run:
         dir = Direction.UP
     elif key[pygame.K_DOWN] == True:
         dir = Direction.DOWN
-    elif key[pygame.K_SPACE] == True:
-        update_time = 1
+    elif key[pygame.K_p] == True:
+        if PAUSE == 1:
+            PAUSE = 0
+        else:
+            PAUSE = 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    FRAME_COUNT += 1
-    if FRAME_COUNT == 500:
-        green_snake.move(dir)
-        if green_snake.out_of_bound() or green_snake.bonk():
-            run = False
-        
-        if green_snake.head.x == apple.x and green_snake.head.y == apple.y:
-            green_snake.eat(dir)
-            apple = random_apple()   
-        
-        pygame.draw.rect(screen, (255, 0, 0), apple)
-        pygame.draw.rect(screen, (255, 255, 0), green_snake.head)
-        for i in green_snake.body:
-            pygame.draw.rect(screen, (0, 255, 0), i)
-        for i in range(0,347):
-            pygame.draw.rect(screen, (0, 0, 255), wall[i])    
+    green_snake.move(dir)
+    if green_snake.out_of_bound() or green_snake.bonk():
+        run = False
+    
+    if green_snake.head.x == apple.x and green_snake.head.y == apple.y:
+        green_snake.eat(dir)
+        apple = random_apple()
+        SCORE += 1   
+    
+    pygame.draw.rect(screen, (255, 0, 0), apple)
+    pygame.draw.rect(screen, (255, 255, 0), green_snake.head)
+    for i in green_snake.body:
+        pygame.draw.rect(screen, (0, 255, 0), i)
+    for i in range(0,428):
+        pygame.draw.rect(screen, (0, 0, 255), wall[i])
 
-        pygame.display.update()
-        FRAME_COUNT = 0
+    score = font.render("SCORE : "+str(SCORE), True, yellow, black)
+    screen.blit(score, (20,560))        
+
+    pygame.display.update()
 
 pygame.quit()
 
