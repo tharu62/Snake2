9f284163eb99acd3165890f827374260ec39c999
tharu62 <tharinduxdeshan@gmail.com>
2025-03-13 14:27:02 +0100

    first pygame implementation

--- snake2.py
+++ snake2.py
@@ -23,11 +23,37 @@ class snake():
     def return_body(self):
         return self.body
 
-def main():
-    h = box(0,0)
-    mySnake = snake(h)
-    print()
+pygame.init()
 
-if __name__ == "__main__":
-    main()
+SCREEN_WIDHT = 800
+SCREEN_HIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HIGHT))
+
+player = pygame.Rect((300, 250, 50, 50))
+
+run = True
+while run:
+
+    screen.fill((0,0,0))
+
+    pygame.draw.rect(screen, (255, 0, 0), player)
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 1)        
+
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.update()
+
+pygame.quit()
 
