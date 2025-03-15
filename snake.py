2d5eba82f85865cd902dcc429b968c4cca629177
tharu62 <tharinduxdeshan@gmail.com>
2025-03-15 11:34:42 +0100

    corrected errors

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+from turtle import down
 import pygame
 from pygame.locals import *
 from enum import Enum
@@ -7,6 +8,7 @@ class Direction(Enum):
     DOWN = 2
     RIGHT = 3
     LEFT = 4
+    NONE = 5
 
 class Snake():
 
@@ -31,19 +33,34 @@ class Snake():
             case Direction.RIGHT:
                 self.head.move_ip(10, 0)
 
-    def eat(self, direction):
-        tail = Rect(self.body[len(self.body)-1].x, self.body[len(self.body)-1].y, 9 ,9)
-        match direction:
-            case Direction.UP:
-                tail.move_ip(0, 10)
-            case Direction.DOWN:
-                tail.move_ip(0, -10)
-            case Direction.LEFT:
-                tail.move_ip(10, 0)
-            case Direction.RIGHT:
-                tail.move_ip(-10, 0)
+    def eat(self):
+        tail = Rect(self.head.x, self.head.y, 9 ,9)   
+        if len(self.body) == 1:         
+            if self.body[0].x == self.head.x:
+                if self.body[0].y > self.head.y:
+                    tail.move_ip(0, -10)
+                else:
+                    tail.move_ip(0, 10)
+            else:
+                if self.body[0].x > self.head.x:
+                    tail.move_ip(10, 0)
+                else:
+                    tail.move_ip(-10, 0) 
+        else:
+            tail.x = self.body[len(self.body)-1].x
+            tail.y = self.body[len(self.body)-1].y
+            if self.body[len(self.body)-1].x == self.body[len(self.body)-2].x:
+                if self.body[len(self.body)-1].y > self.body[len(self.body)-2].y:
+                    tail.move_ip(0, -10)
+                else:
+                    tail.move_ip(0, 10)
+            else:
+                if self.body[len(self.body)-1].x > self.body[len(self.body)-2].x:
+                    tail.move_ip(10, 0)
+                else:
+                    tail.move_ip(-10, 0)    
         self.body.append(tail)
-    
+
     def out_of_bound(self):
         if self.head.x == 400 or self.head.x == 0:
             return True
@@ -57,15 +74,68 @@ class Snake():
                 return True
         return False
     
-    def hunt(self, apple):
-        if apple.x == self.head.x and apple.y == self.head.y:
-            # self.move(Direction.RIGHT)
-            return
-        elif apple.x > self.head.x:
+    def hit_obstacle(self, obstacle):
+        for i in obstacle:
+            if self.head.x == i.x and self.head.y == i.y:
+                return True
+        return False
+
+    def hunt(self, apple, wall, forest):
+        right = True
+        left = True
+        up = True
+        down = True
+        for i in self.body:
+            if self.head.x+10 == i.x and self.head.y == i.y:
+                right = False
+            if self.head.x-10 == i.x and self.head.y == i.y:
+                left = False
+            if self.head.x == i.x and self.head.y-10 == i.y:
+                up = False
+            if self.head.x == i.x and self.head.y+10 == i.y:
+                down = False
+        for i in wall:
+            if self.head.x+10 == i.x and self.head.y == i.y:
+                right = False
+            if self.head.x-10 == i.x and self.head.y == i.y:
+                left = False
+            if self.head.x == i.x and self.head.y-10 == i.y:
+                up = False
+            if self.head.x == i.x and self.head.y+10 == i.y:
+                down = False          
+        for i in forest:
+            if self.head.x+10 == i.x and self.head.y == i.y:
+                right = False
+            if self.head.x-10 == i.x and self.head.y == i.y:
+                left = False
+            if self.head.x == i.x and self.head.y-10 == i.y:
+                up = False
+            if self.head.x == i.x and self.head.y+10 == i.y:
+                down = False              
+        if apple.x > self.head.x and right:
             self.move(Direction.RIGHT)
-        elif apple.x < self.head.x:
+        elif apple.x < self.head.x and left:
             self.move(Direction.LEFT)
-        elif apple.y > self.head.y:
+        elif apple.y > self.head.y and down:
             self.move(Direction.DOWN)
-        elif apple.y < self.head.y:
-            self.move(Direction.UP)    
+        elif apple.y < self.head.y and up:
+            self.move(Direction.UP)
+        elif right:
+            self.move(Direction.RIGHT)
+        elif left:
+            self.move(Direction.LEFT)
+        elif down:
+            self.move(Direction.DOWN)
+        elif up:
+            self.move(Direction.UP)
+    
+    # Implement the A* algorithm to find the shortest path to the apple
+    def A_star_hunt(self, apple, wall, forest):
+        
+        return
+    
+    # Implement the Dijkstra algorithm to find the shortest path to the apple
+    def Dijkstra_hunt(self, apple, wall, forest):
+            
+        return
+    
--- snake2.py
+++ snake2.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+import numpy
 from snake import Snake
 from snake import Direction
 from pygame.locals import *
@@ -11,8 +12,16 @@ def random_apple(n):
         apple = pygame.Rect(0, 0, 9, 9)
     elif n == 2:
         apple = pygame.Rect(410, 0, 9, 9)        
-    apple.move_ip(random.randint(1,39)*10, random.randint(1, 52)*10)
+    apple.move_ip(random.randint(1,37)*10, random.randint(1, 52)*10)
     return apple     
+
+def random_obstacle(n):
+    if n == 1:
+        obstacle = pygame.Rect(0, 0, 9, 9)
+    elif n == 2:
+        obstacle = pygame.Rect(410, 0, 9, 9)        
+    obstacle.move_ip(random.randint(1,38)*10, random.randint(1, 52)*10)
+    return obstacle
 
 pygame.init()
 
@@ -28,6 +37,8 @@ yellow_snake = Snake(pygame.Rect(590, 290, 9, 9))
 apple = pygame.Rect(200, 200, 9, 9)
 rotten_apple = pygame.Rect(600, 200, 9, 9)
 wall = []
+forest = []
+rotten_forest = []
 
 # Set up WALLMARIA
 for i in range(0, 60):
@@ -71,11 +82,12 @@ run = True
 FRAMERATE = 20
 PAUSE = 0
 SCORE = 0
+CPU_SCORE = 0
 while run:
 
     timer.tick(FRAMERATE)
-
     screen.fill((0,0,0))
+
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] == True:
         dir = Direction.LEFT
@@ -85,48 +97,57 @@ while run:
         dir = Direction.UP
     elif key[pygame.K_DOWN] == True:
         dir = Direction.DOWN
-    elif key[pygame.K_p] == True:
-        if PAUSE == 1:
-            PAUSE = 0
-        else:
-            PAUSE = 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    # green_snake.move(dir)
-    # if green_snake.out_of_bound() or green_snake.bonk():
-    #     run = False
-    
-    # if green_snake.head.x == apple.x and green_snake.head.y == apple.y:
-    #     green_snake.eat(dir)
-    #     apple = random_apple(1)
-    #     SCORE += 1   
+    green_snake.move(dir)
+    if green_snake.out_of_bound() or green_snake.bonk() or green_snake.hit_obstacle(forest):
+        run = False
+        break
+
+    if green_snake.head.x == apple.x and green_snake.head.y == apple.y:
+        green_snake.eat()
+        apple = random_apple(1)
+        SCORE += 1   
+
+    yellow_snake.hunt(rotten_apple, wall, rotten_forest)
+    if yellow_snake.head.x == rotten_apple.x and yellow_snake.head.y == rotten_apple.y:
+        rotten_apple = random_apple(2)
+        yellow_snake.eat()
+        forest.append(random_obstacle(1))
+        rotten_forest.append(random_obstacle(2))
+        CPU_SCORE += 1
     
     pygame.draw.rect(screen, (255, 0, 0), apple)
     pygame.draw.rect(screen, (255, 255, 0), green_snake.head)
     for i in green_snake.body:
-        pygame.draw.rect(screen, (0, 255, 0), i)
+        pygame.draw.rect(screen, (0, 200, 0), i)
     
     pygame.draw.rect(screen, (128, 0, 128), rotten_apple)   
     pygame.draw.rect(screen, (255, 0, 255), yellow_snake.head)
     for j in yellow_snake.body:
         pygame.draw.rect(screen, (200, 200, 0), j)
     
-    yellow_snake.hunt(rotten_apple)
+    for i in wall:
+        pygame.draw.rect(screen, (0, 0, 255), i)
+    for i in rotten_forest:
+        pygame.draw.rect(screen, (55, 55, 55), i)
+    for i in forest:    
+        pygame.draw.rect(screen, (0, 100, 0), i)
 
-    if yellow_snake.head.x == rotten_apple.x and yellow_snake.head.y == rotten_apple.y:
-        rotten_apple = random_apple(2)
-        yellow_snake.eat(dir)
-
-    for i in range(0,428):
-        pygame.draw.rect(screen, (0, 0, 255), wall[i])
-
-    score = font.render("SCORE : "+str(SCORE), True, yellow, black)
-    screen.blit(score, (20,555))        
+    score = font.render("SCORE : "+str(SCORE), True, (0, 255, 0), (0, 0, 0))
+    cpu_score = font.render("CPU SCORE : "+str(CPU_SCORE), True, (255, 255, 0), (0, 0, 0))
+    screen.blit(score, (20,555))    
+    screen.blit(cpu_score, (420, 555))    
 
     pygame.display.update()
 
-pygame.quit()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()  
+
 
